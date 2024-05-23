75e606a024431815aaeb8284c07b10990a4f5e07
fc202401 <161753873+fc202401@users.noreply.github.com>
2024-05-23 18:47:09 +0000

    Aula dia 11

---
+++ dia11/frontend/src/services/aprendiz.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+
+const urlApi = "https://futurocodebackend-ureotn5zxq-uc.a.run.app/aprendiz"
+
+class  AprendizService {
+
+   
+
+    listar() {
+        return axios.get(`${urlApi}`)
+    }
+
+    obter(id) {
+        return axios.get(`${urlApi}/${id}`)
+
+    }
+
+    gravar(aprendiz) {
+    
+        if (aprendiz.id) {
+          return axios.put(`${urlApi}/${aprendiz.id}`, aprendiz)
+        } else {
+          return axios.post(`${urlApi}/`, aprendiz)
+        }
+    }
+
+    apagar(id) {
+        return axios.delete(`${urlApi}/${id}`)
+    }
+
+}
+
+export default new AprendizService();
